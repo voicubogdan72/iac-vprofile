b859b27b22cd7c3e93f2a0035b63191fb23ba233
Voicu Bogdan <voicubogdan72@gmail.com>
2024-04-11 18:25:55 +0300

    update

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofileactions72"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "eu-central-1"
   }
 
   required_version = "~> 1.6.3"
